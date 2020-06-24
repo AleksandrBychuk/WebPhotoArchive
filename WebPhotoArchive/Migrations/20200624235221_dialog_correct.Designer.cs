36a62a4b328238abb1e33b751d68b9dbd7a1b457
AlexanderBychuk <36333294+AlexanderBychuk@users.noreply.github.com>
2020-06-25 02:55:33 +0300

    RedesignDialogs and add little things v 1.0

--- WebPhotoArchive/Migrations/20200624235221_dialog_correct.Designer.cs
+++ WebPhotoArchive/Migrations/20200624235221_dialog_correct.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WebPhotoArchive.Models;
@@ -9,9 +10,10 @@ using WebPhotoArchive.Models;
 namespace WebPhotoArchive.Migrations
 {
     [DbContext(typeof(WebArchiveContext))]
-    partial class WebArchiveContextModelSnapshot : ModelSnapshot
+    [Migration("20200624235221_dialog_correct")]
+    partial class dialog_correct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
